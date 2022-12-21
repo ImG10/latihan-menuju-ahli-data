d8a2637dfd9b3aebe11031f36a9699f637b55ca5
Muhammad Giovani <109863707+ImG10@users.noreply.github.com>
2022-12-21 13:01:53 +0800

    10-Tugas-Praktek.py

---
+++ DQLab/Career Track - Data Analyst/Python For Data Professional Beginer - Part 1/Mari Mengenal Python/10-Tugas-Praktek.py
@@ -0,0 +1,11 @@
+## Tugas Praktek
+# Tantangan belum berakhir. Aku diminta Senja untuk membuat program pengurangan sederhana dengan Python.
+
+# Tugas:
+# Deklarasi variable bilangan1 dengan 20, dan bilangan2 dengan 10 dan tampilkan hasil pengurangan bilangan1 & bilangan 2.
+
+# Code Editor
+
+bilangan1 = 20
+bilangan2 = 10
+print(bilangan1 - bilangan2)
