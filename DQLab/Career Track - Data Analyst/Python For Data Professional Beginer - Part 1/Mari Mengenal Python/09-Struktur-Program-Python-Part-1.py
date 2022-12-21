56912cffb6fbea39717f9ddae786eb1626357482
Muhammad Giovani <109863707+ImG10@users.noreply.github.com>
2022-12-21 10:50:18 +0800

    09-Struktur-Program-Python-Part-1.py

---
+++ DQLab/Career Track - Data Analyst/Python For Data Professional Beginer - Part 1/Mari Mengenal Python/09-Struktur-Program-Python-Part-1.py
@@ -0,0 +1,49 @@
+## Struktur Program Python - Part 1
+#Setelah berhasil membuat program sederhana menggunakan Python, aku diminta Senja untuk belajar struktur dari sebuah program Python.
+
+# Dari situ aku paham sebuah program Python pada umumnya terdiri dari:
+
+# 1. Statements: Instruksi yang diberikan secara baris per baris untuk dijalankan oleh mesin. Contoh:
+# Statement
+print("Belajar Python menyenangkan") 
+print("Halo Dunia")
+print("Hello World!")
+
+# 2. Variables: Lokasi penyimpanan yang dapat digunakan untuk menampung sebuah data atau informasi. Contoh: aku mempunyai variabel yang bernama bilangan1, bilangan2, dan kalimat1
+# Variables
+bilangan1 = 5
+bilangan2 = 10
+
+# 3. Literals: Simbol-simbol yang dapat kita gunakan untuk mengisi suatu variabel. Pada kode yang telah dicontohkan di atas, angka 5 dan 10 serta 'Belajar Bahasa Python' disebut sebagai literal.
+# Literals
+kalimat1 = "Belajar Bahasa Python"
+
+# 4. Operators: Simbol-simbol yang dapat digunakan untuk mengubah nilai dari satu variabel dengan melibatkan satu atau lebih variabel dan literal. Contoh: Tanda + merupakan salah satu contoh operator. Dengan menggunakan tanda +, aku berhasil menambahkan isi dari bilangan1 dan bilangan2!
+# Operators
+print(bilangan1 + bilangan2)
+
+# Adapun operator yang lain selain operator + adalah sebagai berikut.
+# 1. Operator - yang berfungsi sebagai operator pengurangan,
+# 2. Operator * yang berfungsi sebagai operator perkalian, dan
+# 3. Operator ** untuk pemangkatan
+
+# Tugas :
+# Aku diminta untuk mencoba mempraktekkan tutorial yang diajarkan untuk memahami struktur bahasa pemrograman Python.
+
+#Code Editor
+
+# Statement
+print("Belajar Python menyenangkan") 
+print("Halo Dunia")
+print("Hello World!")
+# Variables & Literals
+bilangan1 = 5
+bilangan2 = 10
+kalimat1 = "Belajar Bahasa Python"
+# Operators
+print(bilangan1 + bilangan2)
+
+
+ 
+
+ 
