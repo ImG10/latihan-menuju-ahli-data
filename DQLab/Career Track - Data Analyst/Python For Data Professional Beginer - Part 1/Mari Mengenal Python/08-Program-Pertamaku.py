be1d44fb4e63379cb12e6020c183a4f6a58eace4
Muhammad Giovani <109863707+ImG10@users.noreply.github.com>
2022-12-21 10:46:07 +0800

    08-Program-Pertamaku.py

---
+++ DQLab/Career Track - Data Analyst/Python For Data Professional Beginer - Part 1/Mari Mengenal Python/08-Program-Pertamaku.py
@@ -0,0 +1,11 @@
+## Program Pertamaku
+
+# Keren! Aku baru tahu kalau Python memiliki fungsi print() yang berguna untuk menampilkan pesan ke layar pengguna.
+
+# Tugas:
+# Sekarang aku diberikan tugas dari Senja untuk menampilkan kalimat “Halo Dunia” dan “Riset Bahasa Python” menggunakan fungsi print().
+
+# Code Editor
+
+print("Halo Dunia")
+print("Riset Bahasa Python")
